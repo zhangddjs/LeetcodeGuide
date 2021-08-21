9873f8b0e483a67779983c959bf7183c9203bdf2
zhangddjs <1005155946@qq.com>
2021-08-21 15:17:10 +0800

    156-Binary Tree Upside Down

---
+++ mycodes/156-Binary Tree Upside Down.go
@@ -0,0 +1,40 @@
+/**
+ * The description of this problem is not clearly, it didn't say that right nodes with two child wll not do the option.
+ * So here I implement this recursive function which will both operate on left and right nodes who has two childs.
+ *
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func upsideDownBinaryTree(root *TreeNode) *TreeNode {
+    if root == nil || root.Right == nil {
+        return root
+    }
+
+    rightRoot := upsideDownBinaryTree(root.Right)      //get the new root of right subtree
+    left := root.Left      // record the origin left child of current root
+    leftRoot := upsideDownBinaryTree(left)      //get the new root of left subtree
+    left.Right = root
+    left.Left = rightRoot
+    root.Left = nil
+    root.Right = nil
+    return leftRoot      //the new root of left subtree is also the new root of current tree
+}
+
+// AC answer
+func upsideDownBinaryTree(root *TreeNode) *TreeNode {
+    if root == nil || root.Right == nil {
+        return root
+    }
+    
+    left := root.Left      // record the origin left child of current root
+    leftRoot := upsideDownBinaryTree(left)      //get the new root of left subtree
+    left.Right = root
+    left.Left = root.Right
+    root.Left = nil
+    root.Right = nil
+    return leftRoot      //the new root of left subtree is also the new root of current tree
+}
